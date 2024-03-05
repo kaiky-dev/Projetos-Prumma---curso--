4931c2d8cb86262dedc8c3a60dfc66b6bd04e732
Kaiky Vasques <156720570+kaiky-dev@users.noreply.github.com>
2024-03-05 16:03:33 -0300

    UPD1
    Exercíco " 1 " finalizado.

---
+++ JS/Carteira/script.js
@@ -0,0 +1,12 @@
+// Programa de aptidão a carteira de motorista
+
+let idade = parseInt(prompt("Qual a sua idade"));
+
+console.log(idade)
+
+if(idade >= 18){
+    alert("Apto a tirar sua carteira de motorista.")
+}
+else{
+    alert("Não esta apta a carteira de motorista. Volte aos 18 anos.")
+}
